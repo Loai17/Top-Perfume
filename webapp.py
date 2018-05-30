f16c550995817e5e9260455d130485b7380112c5
Loai17 <loai.qubti@gmail.com>
2018-05-30 08:14:40 +0300

    fixed tabs identation for heroku

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ from forms import ContactForm
 
 # Flask Mail
 from flask_mail import Message, Mail
- 
+
 mail = Mail()
 
 UPLOAD_FOLDER = 'static/productsImages'
@@ -24,7 +24,7 @@ app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'loai.qubti@gmail.com'
 app.config["MAIL_PASSWORD"] = 'Qloai1107'
- 
+
 mail.init_app(app)
 
 # LOCAL
@@ -35,34 +35,84 @@ DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def home():
 	form = ContactForm()
+	productsList = session.query(ShopItems).all()
+	loadMore = False
+
+	products=[]
+	if len(productsList) >= 8:
+		for i in range(8) :
+			products.append(productsList[i])
+	else:
+		products = productsList
+
+	if len(products)<len(productsList) :
+		loadMore = True
 
 	if request.method == 'POST':
-	    if form.validate() == False:
-	      	flash('All fields are required.')
-	      	return render_template('index.html', form=form)
-	    else:
-	    	msg = Message("Website Message", sender='contactUs@example.com', recipients=['loai.qubti@gmail.com'])
-      		msg.body = """
-From: %s <%s>
-Subject: %s
-Message: %s
-      		""" % (form.name.data, form.email.data, form.subject.data, form.message.data)
-      		mail.send(msg)
-	      	
-	      	return render_template('index.html', success=True)
-		
-	return render_template('index.html', form=form)
+		if form.validate() == False:
+			flash('All fields are required.')
+			return render_template('index.html', form=form)
+		else:
+			msg = Message("Website Message", sender='contactUs@example.com', recipients=['loai.qubti@gmail.com'])
+			msg.body = """
+			From: %s <%s>
+			Subject: %s
+			Message: %s
+			""" % (form.name.data, form.email.data, form.subject.data, form.message.data)
+			mail.send(msg)
 
-@app.route('/shop', methods=['GET'])
+			return render_template('index.html', success=True)
+
+	return render_template('index.html', form=form,number=1, products=products, loadMore=loadMore)
+
+
+@app.route('/<number>', methods=['GET','POST'])
+def loadMore(number):
+	form = ContactForm()
+	productsList = session.query(ShopItems).all()
+	loadMore = False
+
+	products=[]
+	if len(productsList) >= 8*number:
+		for i in range(8*number) :
+				products.append(productsList[i])
+	else:
+		products = productsList
+
+	if len(products)<len(productsList) :
+		loadMore = True
+
+	if request.method == 'POST':
+		if form.validate() == False:
+			flash('All fields are required.')
+			return render_template('index.html', form=form)
+		else:
+			msg = Message("Website Message", sender='contactUs@example.com', recipients=['loai.qubti@gmail.com'])
+			msg.body = """
+			From: %s <%s>
+			Subject: %s
+			Message: %s
+			""" % (form.name.data, form.email.data, form.subject.data, form.message.data)
+			mail.send(msg)
+
+			return render_template('index.html', success=True)
+		
+	return render_template('index.html', form=form, number=number, products=products, loadMore=loadMore)
+
+@app.route('/shop', methods=['GET','POST'])
 def shop():
 	products = session.query(ShopItems).all()
 	return render_template('shop.html' , products = products)
 
+@app.route('/about', methods=['GET','POST'])
+def about():
+	return render_template('about.html'	)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
